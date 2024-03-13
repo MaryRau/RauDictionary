82d4f8c2e10a274313d5e889451469deff6c07ac
217029 <217029@KB37-1-120-C13>
2024-03-13 10:53:39 +0300

    Добавьте файлы проекта.

---
+++ СловарьТестировщика/AddPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace СловарьТестировщика
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class AddPage : Page
+    {
+        private Dictionary _currentDict = new Dictionary();
+
+        public AddPage()
+        {
+            InitializeComponent();
+
+            DataContext = _currentDict;
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(Term.Text))
+                errors.AppendLine("Укажите понятие!");
+            if (string.IsNullOrWhiteSpace(Opr.Text))
+                errors.AppendLine("Укажите определение!");
+            if (string.IsNullOrWhiteSpace(Sou.Text))
+                errors.AppendLine("Укажите источник!");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentDict.id == 0)
+                DictEntities.GetContext().Dictionary.Add(_currentDict);
+
+            try
+            {
+                DictEntities.GetContext().SaveChanges();
+                MessageBox.Show("Данные успешно сохранены!");
+                NavigationService.Navigate(new DictPage());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new DictPage());
+        }
+
+
+    }
+}
---
+++ СловарьТестировщика/DictPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace СловарьТестировщика
+{
+    /// <summary>
+    /// Логика взаимодействия для DictPage.xaml
+    /// </summary>
+    public partial class DictPage : Page
+    {
+        public DictPage()
+        {
+            InitializeComponent();
+            DataGridTesting.ItemsSource = DictEntities.GetContext().Dictionary.ToList();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddPage());
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                DictEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(x => x.Reload());
+                DataGridTesting.ItemsSource = DictEntities.GetContext().Dictionary.ToList();
+            }
+        }
+
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+            var termsForRemoving = DataGridTesting.SelectedItems.Cast<Dictionary>().ToList();
+
+            if (MessageBox.Show($"Вы точно хотите удалить записи в количестве {termsForRemoving.Count()} элементов?", "Внимание",
+                            MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    DictEntities.GetContext().Dictionary.RemoveRange(termsForRemoving);
+                    DictEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные успешно удалены!");
+
+                    DataGridTesting.ItemsSource = DictEntities.GetContext().Dictionary.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+    }
+}
